a61902e406f6b9c92420e37961f1657ea1cc6d62
shreyas gupta <shreyas.charming@gmail.comm>
2020-06-11 00:54:52 +0530

    Initial Commit

---
+++ gameOfSet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gameOfSet
+//
+//  Created by shreyas gupta on 11/06/20.
+//  Copyright © 2020 shreyas gupta. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
